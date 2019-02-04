a30bd811c19991beae2e147d80174b836ff7fd42
André Piacentini <andre.piacentini@gmail.com>
2019-02-04 12:01:04 -0200

    setting Profile controller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Validators\ValidatesProfileRequests;
 use App\Models\Profile;
 use App\Models\User;
-use App\Transformers\UserTransform;
+use App\Transformers\ProfileTransform;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 
@@ -51,38 +51,33 @@ class ProfileController extends Controller
     public function get($id)
     {
         if (Gate::denies('view', $this->getUserLogged())) {
-            return response()->json(['error' => 'policy: cannot view user data'], 403);
+            return response()->json(['error' => 'policy: cannot view profile data'], 403);
         }
         $profile = Profile::find($id);
         if (!$profile) {
-            return response()->json(['error' => 'user not found'], 404);
+            return response()->json(['error' => 'profile not found'], 404);
         }
-        return $this->response->item($profile, new UserTransform());
+        return $this->response->item($profile, new ProfileTransform());
     }
 
     public function post(Request $request, $rawData = false)
     {
 
         if (Gate::denies('create', $this->getUserLogged())) {
-            return response()->json(['error' => 'policy: cannot create a new user'], 403);
+            return response()->json(['error' => 'policy: cannot create a new profile'], 403);
         }
         $this->validateNew($request);
 
         // create
-        $user = (new User())->store([
-            'name' => $request->post('name'),
-            'username' => $request->post('username'),
-            'password' => $request->post('password'),
-            'cpf' => $request->post('cpf'),
-            'profile_id' => $request->post('profile_id'),
-            'hotel_id' => $request->post('hotel_id'),
+        $profile = (new Profile())->store([
+            'name' => $request->post('name')
         ]);
         
-        if (!$user) {
-            return response()->json(['error' => 'an error occurred while trying to create a user', 'error_list' => $user->getErrors()], 404);
+        if (!$profile) {
+            return response()->json(['error' => 'an error occurred while trying to create a profile', 'error_list' => $profile->getErrors()], 404);
         }
         
-        return $rawData ? $user : $this->response->item($user, new UserTransform());
+        return $rawData ? $profile : $this->response->item($profile, new ProfileTransform());
         
     }
 
@@ -90,27 +85,22 @@ class ProfileController extends Controller
     {
 
         if (Gate::denies('update', $this->getUserLogged())) {
-            return response()->json(['error' => 'policy: cannot update an existent user'], 403);
+            return response()->json(['error' => 'policy: cannot update an existent profile'], 403);
         }
         $request['id'] = $id;
         $this->validateUpdate($request);
 
         // create
-        $user = (new User())->put([
+        $profile = (new Profile())->put([
             'id' => $id,
-            'name' => $request->post('name'),
-            'username' => $request->post('username'),
-            'password' => $request->post('password'),
-            'cpf' => $request->post('cpf'),
-            'profile_id' => $request->post('profile_id'),
-            'hotel_id' => $request->post('hotel_id'),
+            'name' => $request->post('name')
         ]);
 
-        if (!$user) {
-            return response()->json(['error' => 'an error occurred while trying to create a user', 'error_list' => $user->getErrors()], 404);
+        if (!$profile) {
+            return response()->json(['error' => 'an error occurred while trying to create a profile', 'error_list' => $profile->getErrors()], 404);
         }
 
-        return $this->response->item($user, new UserTransform());
+        return $this->response->item($profile, new ProfileTransform());
 
     }
 
@@ -118,17 +108,17 @@ class ProfileController extends Controller
     {
 
         if (Gate::denies('delete', $this->getUserLogged())) {
-            return response()->json(['error' => 'policy: cannot delete an existent user'], 403);
+            return response()->json(['error' => 'policy: cannot delete an existent profile'], 403);
         }
 
         // create
-        $user = (new User())->del($id);
+        $profile = (new Profile())->del($id);
 
-        if (!$user) {
-            return response()->json(['error' => 'an error occurred while trying to create a user', 'error_list' => $user->getErrors()], 404);
+        if (!$profile) {
+            return response()->json(['error' => 'an error occurred while trying to create a profile', 'error_list' => $profile->getErrors()], 404);
         }
 
-        return $this->response->item($user, new UserTransform());
+        return $this->response->item($profile, new ProfileTransform());
 
     }
 }
--- app/Http/Validators/ValidatesProfileRequests.php
+++ app/Http/Validators/ValidatesProfileRequests.php
@@ -10,24 +10,15 @@ trait ValidatesProfileRequests
     protected function validateNew(Request $request)
     {
         $this->validate($request, [
-            'name'          => 'required|string|min:3',
-            'username'      => 'required|email|unique:users,username',
-            'password'      => 'required|string|min:8',
-            'cpf'           => 'required|digits:11',
-            'profile_id'    => 'required|exists:profiles,id',
-            'hotel_id'      => 'required|exists:hotels,id',
+            'name'          => 'required|string|min:3'
         ]);
     }
 
     protected function validateUpdate(Request $request)
     {
         $this->validate($request, [
-            'id'            => 'required|exists:users,id',
-            'name'          => 'required|string|min:3',
-            'password'      => 'required|string|min:8',
-            'cpf'           => 'required|digits:11',
-            'profile_id'    => 'required|exists:profiles,id',
-            'hotel_id'      => 'required|exists:hotels,id',
+            'id'            => 'required|exists:profiles,id',
+            'name'          => 'required|string|min:3'
         ]);
     }
 
